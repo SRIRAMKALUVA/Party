71e5a63cc26320b01be55d7337cfa86942fcda63
sriram <sriram.gupta@planetspark.in>
2021-08-06 13:50:04 +0530

    Minor Edit

--- 1_SQL/Query/20210806_sql_mondai_4.sql
+++ 1_SQL/Query/20210806_sql_mondai_4.sql
@@ -1,4 +1,4 @@
-select member_cd as 会員CD, count(tpm.party_id) as  パーティー参加回数  
-    from t_member as tm 
+SELECT member_cd as 会員CD, count(tpm.party_id) as  パーティー参加回数  
+    FROM t_member as tm 
         LEFT JOIN t_party_member as tpm on(tm.member_id = tpm.member_id) 
             GROUP BY member_cd;
