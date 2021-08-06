5236503a62f4fec6df4e05cdb4d0ef59f4093649
sriram <sriram.gupta@planetspark.in>
2021-08-06 13:47:55 +0530

    Added output

--- 1_SQL/Query/20210806_sql_modai_5.sql
+++ 1_SQL/Query/20210806_sql_modai_5.sql
@@ -1,3 +1,5 @@
 SELECT tm.member_cd AS 会員CD, count(tpm.party_id) AS パーティー参加回数 
-FROM t_member as tm LEFT JOIN t_party_member as tpm on(tm.member_id = tpm.member_id) where tm.member_id not in (select member_id from t_party_member)
-GROUP BY tm.member_cd;
+	FROM t_member as tm 
+		LEFT JOIN t_party_member as tpm on(tm.member_id = tpm.member_id) 
+			where tm.member_id not in (select member_id from t_party_member)
+				GROUP BY tm.member_cd;
--- 1_SQL/Query/20210806_sql_mondai_4.sql
+++ 1_SQL/Query/20210806_sql_mondai_4.sql
@@ -1,3 +1,4 @@
-SELECT tm.member_cd AS 会員CD, count(tpm.member_id) AS パーティー参加回数 
-    FROM t_member as tm, t_party_member as tpm WHERE tm.member_id = tpm.member_id
-        GROUP BY tm.member_cd;
+select member_cd as 会員CD, count(tpm.party_id) as  パーティー参加回数  
+    from t_member as tm 
+        LEFT JOIN t_party_member as tpm on(tm.member_id = tpm.member_id) 
+            GROUP BY member_cd;
