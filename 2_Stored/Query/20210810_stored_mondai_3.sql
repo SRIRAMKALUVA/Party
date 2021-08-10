bcdf335c24ce02b0135cc5dc98ff285631466f0e
sriram <sriram.gupta@planetspark.in>
2021-08-10 11:51:44 +0530

    Stored Mondai 3 completed

---
+++ 2_Stored/Query/20210810_stored_mondai_3.sql
@@ -0,0 +1,22 @@
+CREATE or REPLACE function mondai_3() RETURNS TABLE (
+パーティーID bigint,
+パーティー名 text,
+開催日時 text
+)
+ as $$
+    BEGIN
+      RETURN QUERY 
+	  	select distinct t_party.party_id as パーティーID,title as パーティー名,to_char(start_ts, 'YYYY/MM/DD  HH24:MI') as 開催日時 
+		from t_party_member 
+ 		INNER JOIN t_party on(t_party_member.party_id = t_party.party_id)
+ 		INNER JOIN t_member on(t_party_member.member_id = t_member.member_id)
+ 		where gender_kbn = '00101' 
+ 		and 
+ 		t_party_member.party_id not in 
+ 		(select party_id from t_party_member 
+  			INNER JOIN t_member on(t_party_member.member_id = t_member.member_id) 
+  				where gender_kbn = '00102');
+    END $$ 
+language plpgsql;
+select * from mondai_3();
+
