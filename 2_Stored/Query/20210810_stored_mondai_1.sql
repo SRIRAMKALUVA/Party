e3d749028ed1e51773397bea8aca01dc8eed9dd8
sriram <sriram.gupta@planetspark.in>
2021-08-10 13:11:18 +0530

    Added variable function code

--- 2_Stored/Query/20210810_stored_mondai_1.sql
+++ 2_Stored/Query/20210810_stored_mondai_1.sql
@@ -17,3 +17,25 @@ CREATE or REPLACE function mondai_1() RETURNS TABLE (
 language plpgsql;
 
 select * from mondai_1();
+
+
+-- Function with variable
+CREATE or REPLACE function mondai_1(members bigint) RETURNS TABLE (
+  会員CD text,
+  性別区分 text,
+  名前（姓 text,
+  誕生日 date)
+ as $$
+ 
+    BEGIN
+      RETURN QUERY 
+	  	SELECT member_cd as 会員CD,
+        gender_kbn as 性別区分,
+        family_kj as 名前（姓,
+		birthday_ts::timestamp::date as 誕生日 
+		from t_member 
+        where t_member.member_id in (members);
+    END $$ 
+language plpgsql;
+
+select * from mondai_1(3);
