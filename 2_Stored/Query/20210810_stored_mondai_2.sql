b757dc3727be36cfc3a9877c4e1f432b670d8c38
sriram <sriram.gupta@planetspark.in>
2021-08-10 13:17:50 +0530

    Variable function code added

--- 2_Stored/Query/20210810_stored_mondai_2.sql
+++ 2_Stored/Query/20210810_stored_mondai_2.sql
@@ -19,3 +19,27 @@ as $$
 language plpgsql;
 
 SELECT *from mondai_2();
+
+
+-- Function with variables
+CREATE OR REPLACE function mondai_2(gender text, Min_age int) RETURNS TABLE(
+会員ID bigint,
+会員CD text,
+性別 text,
+名前_姓名かな text,
+年齢 double precision)
+as $$
+	BEGIN
+		RETURN Query 
+			select 
+				member_id as 会員ID,
+				member_cd as 会員CD,
+                CASE WHEN gender_kbn = '00101' THEN 'Male' WHEN gender_kbn = '00102' THEN 'Female' END 性別,
+                concat(family_kn,' ',first_kn) as 名前_姓名かな,
+                EXTRACT(YEAR FROM age(now(),birthday_ts)) as 年齢 
+                from t_member 
+                where EXTRACT(YEAR FROM age(now(),birthday_ts))>Min_age and gender_kbn = gender;
+    END $$
+language plpgsql;
+
+SELECT *from mondai_2('00102',30);
