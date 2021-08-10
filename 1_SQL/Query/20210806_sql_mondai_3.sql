ca1e7322e71cb9cda4e1806b318358c21107cf2e
sriram <sriram.gupta@planetspark.in>
2021-08-10 13:44:06 +0530

    Modified query a little bit

--- 1_SQL/Query/20210806_sql_mondai_3.sql
+++ 1_SQL/Query/20210806_sql_mondai_3.sql
@@ -5,6 +5,6 @@ from t_party_member
  where gender_kbn = '00101' 
  and 
  t_party_member.party_id not in 
- (select party_id from t_party_member 
-  	INNER JOIN t_member on(t_party_member.member_id = t_member.member_id) 
-  		where gender_kbn = '00102');
+ (select party_id from t_party_member,t_member 
+ 	where t_party_member.member_id = t_member.member_id 
+	 and gender_kbn = '00102');
