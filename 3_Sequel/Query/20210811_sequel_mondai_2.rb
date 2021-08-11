996690e7a240bdf176d6f6fc36d15a74d92dceef
sriram <sriram.gupta@planetspark.in>
2021-08-11 12:49:54 +0530

    Sequel Mondai 2 completed

---
+++ 3_Sequel/Query/20210811_sequel_mondai_2.rb
@@ -0,0 +1,7 @@
+require 'sequel'
+require 'date'
+DB = Sequel.postgres('Party',user:'postgres',password:'root@123',host:'localhost',port:5433,max_connections: 10)
+
+t_member = DB[:t_member]
+
+puts t_member.select(Sequel.as(:member_cd,:会員CD), Sequel.as(Sequel.case({{gender_kbn: ['00101']} => 'male'},'female'),:性別), Sequel.as(Sequel.join([:family_kn, ' ', :first_kn]),:名前_姓名かな), Sequel.as(Date.today.year - Sequel.extract(:year,:birthday_ts),:年齢)).where(Date.today.year - Sequel.extract(:year,:birthday_ts) >= 30).where(:gender_kbn => '00102').all
