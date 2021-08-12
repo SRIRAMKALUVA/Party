c881656e08a08443acda124ef37d543c2006b257
sriram <sriram.gupta@planetspark.in>
2021-08-12 09:55:59 +0530

    Formatted mondai 4

--- 3_Sequel/Query/20210812_sequel_mondai_4.rb
+++ 3_Sequel/Query/20210812_sequel_mondai_4.rb
@@ -3,4 +3,7 @@ require 'date'
 DB = Sequel.postgres('Party',user:'postgres',password:'root@123',host:'localhost',port:5433,max_connections: 10)
 
 t_member = DB[:t_member]
-puts t_member.left_join(:t_party_member, :member_id=>:member_id).select_group(Sequel.as(:member_cd,:会員CD)).select_more{Sequel.as(count(:party_id),:パーティー参加回数)}.all
+puts t_member.left_join(:t_party_member, :member_id=>:member_id)
+    .select_group(Sequel.as(:member_cd,:会員CD))
+    .select_more{Sequel.as(count(:party_id),:パーティー参加回数)}
+    .all
