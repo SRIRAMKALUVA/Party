6d0426d46aa5ed169dbd9ad7128b428d0026d285
sriram <sriram.gupta@planetspark.in>
2021-08-12 10:00:35 +0530

    Formatted mondai 1,2

--- 3_Sequel/Query/20210811_sequel_mondai_1.rb
+++ 3_Sequel/Query/20210811_sequel_mondai_1.rb
@@ -1,7 +1,8 @@
 require 'sequel'
 DB = Sequel.postgres('Party',user:'postgres',password:'root@123',host:'localhost',port:5433,max_connections: 10)
 
-
 t_member = DB[:t_member]
 
-puts t_member.select(Sequel.as(:member_cd,:会員CD), Sequel.as(:gender_kbn,:性別区分), Sequel.as(:family_kj,:名前_姓), Sequel.as(Sequel.cast(:birthday_ts, :date),:誕生日)).where(:member_id => [2,3,4]).all
+puts t_member
+    .select(Sequel.as(:member_cd,:会員CD), Sequel.as(:gender_kbn,:性別区分), Sequel.as(:family_kj,:名前_姓), Sequel.as(Sequel.cast(:birthday_ts, :date),:誕生日))
+    .where(:member_id => [2,3,4]).all
--- 3_Sequel/Query/20210811_sequel_mondai_2.rb
+++ 3_Sequel/Query/20210811_sequel_mondai_2.rb
@@ -4,4 +4,8 @@ DB = Sequel.postgres('Party',user:'postgres',password:'root@123',host:'localhost
 
 t_member = DB[:t_member]
 
-puts t_member.select(Sequel.as(:member_cd,:会員CD), Sequel.as(Sequel.case({{gender_kbn: ['00101']} => 'male'},'female'),:性別), Sequel.as(Sequel.join([:family_kn, ' ', :first_kn]),:名前_姓名かな), Sequel.as(Date.today.year - Sequel.extract(:year,:birthday_ts),:年齢)).where(Date.today.year - Sequel.extract(:year,:birthday_ts) >= 30).where(:gender_kbn => '00102').all
+puts t_member
+    .select(Sequel.as(:member_cd,:会員CD), Sequel.as(Sequel.case({{gender_kbn: ['00101']} => 'male'},'female'),:性別), Sequel.as(Sequel.join([:family_kn, ' ', :first_kn]),:名前_姓名かな), Sequel.as(Date.today.year - Sequel.extract(:year,:birthday_ts),:年齢))
+    .where(Date.today.year - Sequel.extract(:year,:birthday_ts) >= 30)
+    .where(:gender_kbn => '00102')
+    .all
