748a308a39efb923ed9c30639cf2460ca47ee0a1
sriram <sriram.gupta@planetspark.in>
2021-08-13 14:21:17 +0530

    Updated the query for mondai 3

--- 2_Stored/Query/20210810_stored_mondai_3.sql
+++ 2_Stored/Query/20210810_stored_mondai_3.sql
@@ -6,16 +6,17 @@ CREATE or REPLACE function mondai_3() RETURNS TABLE (
  as $$
     BEGIN
       RETURN QUERY 
-	  	select distinct t_party.party_id as パーティーID,title as パーティー名,to_char(start_ts, 'YYYY/MM/DD  HH24:MI') as 開催日時 
-		from t_party_member 
- 		INNER JOIN t_party on(t_party_member.party_id = t_party.party_id)
- 		INNER JOIN t_member on(t_party_member.member_id = t_member.member_id)
- 		where gender_kbn = '00101' 
- 		and 
- 		t_party_member.party_id not in 
- 		(select party_id from t_party_member 
-  			INNER JOIN t_member on(t_party_member.member_id = t_member.member_id) 
-  				where gender_kbn = '00102');
+	  	SELECT 
+			tp.party_id AS パーティーID,
+			tp.title AS パーティー名,
+			to_char(start_ts, 'YYYY/MM/DD  HH24:MI') AS 開催日時 
+			FROM t_party AS tp
+			INNER JOIN t_party_member AS tpm ON (tp.party_id = tpm.party_id)
+			INNER JOIN t_member AS tm ON (tpm.member_id = tm.member_id)
+			GROUP BY tp.party_id
+			HAVING 
+				SUM(case when tm.gender_kbn = '00102'THEN 1 ELSE 0 END) = 0
+		;
     END $$ 
 language plpgsql;
 select * from mondai_3();
